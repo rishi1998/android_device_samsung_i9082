bb4b2fd2b29dc93947551871744cfb69e76da4f3
rishi1998 <freshtech100@gmail.com>
2015-07-15 00:43:25 -0400

    house keeping
    Signed-off-by: rishi1998 <freshtech100@gmail.com>

--- omni.mk
+++ omni.mk
@@ -1,15 +1,15 @@
 # Release name
 PRODUCT_RELEASE_NAME := GalaxyGrandDuos
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9082/device_i9082.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9082
-PRODUCT_NAME := cm_i9082
+PRODUCT_NAME := omni_i9082
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-I9082
